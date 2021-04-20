5f98e8b25fe68511b23fc741e88071ad8e69e77b
Kayum Amzad <kayum2@gmail.com>
2021-04-20 16:49:10 -0400

    Adding new file

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const recipeModel = require('./api/recipe.model');
+const recipeControllers = require('./api/recipe.controllers');
+const app = express();
+require('dotenv').config();
+// const dataBaseURL = process.env.DATABASE;
+const dataBaseURL = process.env.DB_URL || 'mongodb://localhost:27017';
+
+mongoose
+  .connect(dataBaseURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDb connected'))
+  .catch((err) => console.log(err));
+app.use(fileUpload());
+app.use(express.static('static'));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/static/index.html');
+});
+
+app.get('/api/recipes', recipeControllers.findAll);
+app.get('/api/recipes/:id', recipeControllers.findById);
+app.post('/api/recipes', recipeControllers.add);
+app.put('/api/recipes/:id', recipeControllers.update);
+app.delete('/api/recipes/:id', recipeControllers.delete);
+app.get('/api/import', recipeControllers.import);
+app.get('/api/killall', recipeControllers.killall);
+app.post('/api/upload', recipeControllers.upload);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
